0e31608017f885e2579622ebf2dfd857c909c6a7
Pedro Conde <pconde007@gmail.com>
2023-04-18 15:34:27 +0100

    Video 60 de la sección 4

--- src/main/java/com/bolsadeideas/springboot/form/app/controllers/FormController.java
+++ src/main/java/com/bolsadeideas/springboot/form/app/controllers/FormController.java
@@ -1,8 +1,5 @@
 package com.bolsadeideas.springboot.form.app.controllers;
 
-import java.util.HashMap;
-import java.util.Map;
-
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
@@ -29,13 +26,7 @@ public class FormController {
 	public String procesar(@Valid Usuario usuario, BindingResult result, Model model) {
 		model.addAttribute("titulo", "Resultado form");
 		
-		if(result.hasErrors()) {
-			Map<String, String> errores = new HashMap<>();
-			result.getFieldErrors().forEach(err ->{
-				errores.put(err.getField(), "El campo ".concat(err.getField()).concat(" ").concat(err.getDefaultMessage()));
-			});
-			model.addAttribute("error", errores);
-			
+		if(result.hasErrors()) {			
 			return "form";
 		}		
 		
